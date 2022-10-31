bf5a51e7ae0e9e1b7decabbc93c3f642d90447f8
Juliana Dantass <julianadantas498@gmail.com>
2022-10-31 18:38:50 -0300

    up

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,15 @@
 #include <vector>
 #include <iterator>
 #include <list>
-using namespace std;
-
 #include "data.h"
 #include "hungarian.h"
 
+using namespace std;
 
 struct Node {     //estrutura para cada nó da árvore
 
 	std::vector<pair<int, int>> forbidden_arcs;     //lista de arcos proibidos
-	std::vector<std::vector<int>> subtour;          //contem os subtours da solucao
+	std::vector<std::vector<int>> subtours;          //contem os subtours da solucao
 
 	double lower_bound;             //custo total da solucao do algoritmo hungaro
 	int chosen_subtour;             // indice do menor subtour (que será escolhido)
@@ -19,44 +18,64 @@ struct Node {     //estrutura para cada nó da árvore
  };
 
 
-void BnB (){
+// void BnB (){
 
-	tree = std::list<Node>;
-	tree.push_back(root);
-	double upper_bound = std::numeric_limits::infinity<double>();
+// 	tree = std::list<Node>;
 
-	while (!tree.empty()){
+// 	tree.push_back(root);
 
-		auto node = chooseNode();
-		vector<vector<int>> subtour = getSolutionHungarian(*node);
+// 	double upper_bound = std::numeric_limits::infinity<double>();
+
+// 	while (!tree.empty()){
+
+// 		auto node = chooseNode();
+// 		vector<vector<int>> subtour = getSolutionHungarian(*node);
 	
-		if (node->lower_bound > upper_bound){
-			tree.erase(node);
-			continue;
-		}
+// 		if (node->lower_bound > upper_bound){
+// 			tree.erase(node);
+// 			continue;
+// 		}
 
-		if (node->feasible){
-			upper_bound = min(upper_bound, node->lower_bound);
-		}
+// 		if (node->feasible){
+// 			upper_bound = min(upper_bound, node->lower_bound);
+// 		}
 		
-		/* Gerando os filhos do no raiz */
-		for(int i = 0; i < node.subtour[root.chosen].size() - 1; i++){ // iterarpor todos os arcos do subtour escolhido
-			Node n;
-			n.arcos_proibidos = raiz.arcos_proibidos;
+// 		/* Gerando os filhos do no raiz */
+// 		for(int i = 0; i < node.subtour[root.chosen].size() - 1; i++){ // iterar por todos os arcos do subtour escolhido
+// 			Node n;
+// 			n.arcos_proibidos = raiz.arcos_proibidos;
 
-		std::pair<int, int> forbidden_arc= {node.subtour[root.chosen][i], node.subtour[root.chosen][i+1]};
+// 			std::pair<int, int> forbidden_arc= {node.subtour[root.chosen][i], node.subtour[root.chosen][i+1]};
 
-		n.forbidden_arcs.push_back(forbidden_arc);
-		arvore.push_back(n);
-		}
+// 			n.forbidden_arcs.push_back(forbidden_arc);
+// 			arvore.push_back(n);
+// 		}
 		
-		tree.erase(node);
-	}
+// 		tree.erase(node);
+// 	}
+	
+// }
+
+
+int ExtractingSubtours(int last_node, hungarian_problem_t &p){     //funcao para ajudar a extrair os subtours 
+
+	last_node= -1;
+
+	for(int j = 0; j < p->num_cols; j++){
+
+		if(p->assignment[last_node][j] == 1){
+
+			last_node = j;
+		}
+	} 
 	
 }
 
 
 int main(int argc, char** argv) {      
+
+	std::list<Node> tree;     //declaracao da variavel da arvore
+	Node root;
 
 	//-------- chamando o algoritmo hungaro -----------//
 
@@ -64,6 +83,7 @@ int main(int argc, char** argv) {
 	data->readData();
 
 	double **cost = new double*[data->getDimension()];
+
 	for (int i = 0; i < data->getDimension(); i++){
 		cost[i] = new double[data->getDimension()];
 		for (int j = 0; j < data->getDimension(); j++){
@@ -75,19 +95,56 @@ int main(int argc, char** argv) {
 	int mode = HUNGARIAN_MODE_MINIMIZE_COST;
 	hungarian_init(&p, cost, data->getDimension(), data->getDimension(), mode); // Carregando o problema
 
+
 	double obj_value = hungarian_solve(&p);
 	cout << "Obj. value: " << obj_value << endl;
 
 	cout << "Assignment" << endl;
 	hungarian_print_assignment(&p);
 
+	//----------------------------------------------//
+
+
+	//cout << "ola " << p.assignment[1][0];
+	//exit(0);
+
+
+	vector<int> tour;              //1 possibilidade de tour
+	vector<int> inicial_nodes;
+	int last_node= 0; 
+
+	
+	for (int i = 0; i < p.num_rows; i++){                          //tentando extrair os subtours do assignment
+
+		flag= 0;
+
+		for (int j = 0; j < p.num_cols; j++){
+
+			if(p.assignment[i][j] == 1){
+
+				inicial_nodes.push_back(i);
+				while(1){
+					
+					last_node= j;
+					if(ExtractingSubtours(last_node, p)){
+
+					}else{
+						break;
+					}
+
+				}
+			}
+		}
+	}
+
+
+	//----------- deletando memória ------------------//
+
 	hungarian_free(&p);
 	for (int i = 0; i < data->getDimension(); i++) delete [] cost[i];
 	delete [] cost;
 	delete data;
-
-	//----------------------------------------------//
-
+	//------------------------------------------------//
 
 	return 0;
 }
