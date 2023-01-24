9ec419be1753bd43e39a0ff78e1da4bab52314f9
JulianaDantass <julianadantas498@gmail.com>
2023-01-24 12:55:30 -0300

    up

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ void ExcludingNodes(vector<int> &nodes, vector<int> tour_to_exclue){      //func
 }
 
 vector<vector<int>> GetSubtours(hungarian_problem_t pointer){            //funcao para separar os subtours
-
+																		 //issaq ta bichado!!!!!!1
 	int out;
 	int last_node;
 	vector<int> total_nodes; 
@@ -66,7 +66,7 @@ vector<vector<int>> GetSubtours(hungarian_problem_t pointer){            //funca
 					}
 
 					last_node = j;
-
+					z= 0;
 					break;
 				}
 			}
@@ -326,4 +326,62 @@ int main(int argc, char** argv) {
 			cost[i][j] = data->getDistance(i,j);
 		}
 	}
+
+vector<vector<int>> GetSubtours(hungarian_problem_t pointer){            //funcao para separar os subtours
+																		 //issaq ta bichado!!!!!!1
+	int out;
+	int last_node;
+	vector<int> total_nodes; 
+	vector<vector<int>> total_subtours;
+
+	for (int i = 0; i < pointer.num_rows; i++){                          
+		
+		total_nodes.push_back(i);
+	}
+
+	while(1){                                                 
+
+		vector<int> tour;
+
+		last_node = total_nodes[0];                //definindo o nó inicial
+
+		for(int z = 0; z < total_nodes.size(); z++){
+
+			int i = total_nodes[z]; 
+
+			for (int j = 0; j < pointer.num_cols; j++){                         
+
+				if(pointer.assignment[i][j] == 1 and (last_node == i) ){
+
+					tour.push_back(i+1);
+
+					if(j == tour[0]-1){                  //se a estação atual for igual a estação inicial, significa q o tour terminou
+						tour.push_back(tour[0]);
+					}
+
+					last_node = j;
+
+					break;
+				}
+			}
+    	}
+
+		if( total_nodes.empty() ){             //se nao tem mais nós a pecorrer
+			break;
+
+		}else{
+			total_subtours.push_back(tour);              //adicionando o tour a lista de subtours
+			ExcludingNodes(total_nodes, tour);
+		}
+
+		//getchar();
+	}
+
+	return total_subtours;
+}
+
+
+
+
+
 	*/
